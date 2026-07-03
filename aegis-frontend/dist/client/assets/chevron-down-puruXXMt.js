287836cc857070c902aedfb25d35a89b9fcd1a5f
Harshad Ade <harshadade-extc@atharvacoe.ac.in>
2026-07-03 15:30:24 +0530

    Fix Vite build for deployment

--- aegis-frontend/dist/client/assets/chevron-down-puruXXMt.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-Daz-5QLx.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]);export{t};
---
+++ aegis-frontend/dist/client/assets/validation-CXkIXJ-_.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-n5LQ9ujS.js";import{v as t}from"./index-CqlsJzvj.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
